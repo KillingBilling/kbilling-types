82b1ca13c8c41f81c9ce6df725be9b765ced588f
Ivan Mikushin <i.mikushin@gmail.com>
2013-12-13 17:23:52 +0600

    simplify aggregation function type

--- build.sbt
+++ build.sbt
@@ -5,7 +5,3 @@ name := "kbilling-types"
 version := "0.3.0-SNAPSHOT"
 
 scalaVersion := "2.10.3"
-
-libraryDependencies ++= Seq(
-  "com.github.nscala-time" %% "nscala-time" % "0.6.0"
-)
--- src/main/scala/kbilling/model/BillingPlan.scala
+++ src/main/scala/kbilling/model/BillingPlan.scala
@@ -1,7 +1,6 @@
 package kbilling.model
 
 import BillingPlan.Vars
-import com.github.nscala_time.time.Imports._
 
 object BillingPlan {
   type Vars = Map[String, BigDecimal]
@@ -20,7 +19,7 @@ case class ServiceAccount(aggregates: Map[String, Aggregate]) extends Account
 case class PaymentAccount(cost: Vars => BigDecimal) extends Account
 
 case class Aggregate(
-  aggr: (BigDecimal, BigDecimal, DateTime) => BigDecimal = {(a, b, _) => a + b},
+  aggr: (BigDecimal, BigDecimal) => BigDecimal = {(a, b) => a + b},
   init: Option[BigDecimal] => BigDecimal = {_ => 0}
 )
 
